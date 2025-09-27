6631e309f87fd3eb86add314f9468b11ec9188da
Qi-Month <dy757410@163.com>
2025-09-27 18:52:59 +0800

    rods tag

---
+++ src/main/java/top/qm/industrialplatform/IPTags.java
@@ -0,0 +1,20 @@
+package top.qm.industrialplatform;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class IPTags {
+    public static class Items {
+        public static TagKey<Item> createTag(String namespace, String name) {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(namespace, name));
+        }
+
+        public static TagKey<Item> RODS;
+
+        static {
+            RODS = createTag("forge", "rods");
+        }
+    }
+}
