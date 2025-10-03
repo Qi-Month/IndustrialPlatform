1cd99852a68e059ff257a69ff21692934176d372
VechniMetel <3512546747@qq.com>
2025-10-03 13:52:20 +0800

    Update IndustrialPlatformBlock.java

--- src/main/java/top/qm/industrialplatform/block/custom/platform/IndustrialPlatformBlock.java
+++ src/main/java/top/qm/industrialplatform/block/custom/platform/IndustrialPlatformBlock.java
@@ -31,7 +31,6 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemp
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.pathfinder.PathComputationType;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
